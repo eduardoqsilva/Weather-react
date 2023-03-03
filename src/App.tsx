001b01ab643366b8376fda7284b54f08fdf6beea
eduardoqsilva <eduadsilva@outlook.com>
2023-03-03 00:32:44 -0300

    create request axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import axios from "axios"
+import { useQuery } from "react-query"
 
+const key = import.meta.env.VITE_REACT_APP_API_KEY
 
 function App() {
 
+  const { data } = useQuery('weather', async () => {
+    const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=auto:ip&aqi=no&lang=pt`)
+    return response.data
+  })
+
   return (
-    <h1>Hello, World!</h1>
+    <p>{JSON.stringify(data)}</p>
   )
 }
 
