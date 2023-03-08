0c9d023bcbe0714fb1adc8470e67ccf35429c313
eduardoqsilva <eduadsilva@outlook.com>
2023-03-07 23:50:48 -0300

    update main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,13 +1,47 @@
-import { ReactNode } from "react";
-import { MainContainerStyled } from "./main.styled";
+import { ReactNode, useEffect, useState } from "react";
+import { BackgroundMainStyled, MainContainerStyled } from "./main.styled";
+
+import dayVid from "/videos/day.mp4"
+import nightVid from "/videos/night.mp4"
+import thumbDay from "/thumb/day.png"
+import thumbNight from "/thumb/night.png"
 
 interface mainType {
   children: ReactNode
+  Day: number
 }
-export function Main({ children }: mainType) {
+export function Main({ Day, children }: mainType) {
+  
+  const [isDay , setIsday] = useState(0)
+
+  useEffect(() =>{
+    if(Day !== undefined) {
+      setIsday(Day)
+    }
+  }, [Day])
+  useEffect(() => {
+    if(Day !== undefined) {
+      setIsday(Day)
+    }
+  }, [])
+
   return(
-    <MainContainerStyled>
-      {children}
-    </MainContainerStyled>
+
+    <BackgroundMainStyled>
+      <div className="videos">
+        <video 
+          autoPlay
+          muted
+          loop
+          poster={ Day === 0 ? thumbNight : thumbDay}
+          src={Day === 0 ? nightVid : dayVid}>
+        </video>
+        
+      </div>
+      
+      <MainContainerStyled>
+        {children}
+      </MainContainerStyled>
+    </BackgroundMainStyled>
   )
 }
--- src/components/main/main.styled.ts
+++ src/components/main/main.styled.ts
@@ -6,7 +6,33 @@ export const MainContainerStyled = styled.main`
   
   display: grid;
   grid-template-columns: 100%;
-  grid-template-rows: 90px auto;
+  grid-template-rows: 80px auto 80px;
   align-items: center;
   justify-items: center;
+
+  padding: 1rem;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  overflow-x: hidden;
+  overflow-y: hidden;
+`
+
+export const BackgroundMainStyled = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+
+  & .videos {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+
+    & video {
+      min-width: 100%;
+      min-height: 100%;
+    }
+  }
 `
