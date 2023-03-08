d528a3900a69fadfdeae450946a5ae57f981c5b3
eduardoqsilva <eduadsilva@outlook.com>
2023-03-07 23:54:07 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react"
 import { dataFetching } from "./api/dataFetching"
 import { InputSearch } from "./components/inputSearch"
+import { Loading } from "./components/loading"
+import { Main } from "./components/main"
 import { Weather } from "./components/weather"
 
 
 function App() {
   const [search, setSearch] = useState(String)
-  
+
   //fazendo requisição api
   const res = dataFetching(search)
   console.log(res.isFetching ? 'carregando' : res.data)
   const isFetching = res.isFetching
   const data = res.data
+  
+  
+
+ 
 
   //atualizar os dados quando pesquisar
   useEffect(() => {
@@ -20,15 +26,17 @@ function App() {
 
   return (
     <> 
-      <InputSearch setSearch={setSearch}/>
-      {/* <h2>{isFetching ? 'carregando...' : ''}</h2>
-      <h1>{data?.current.is_day} dia?</h1> */}
-      <Weather 
-        urlIcon={data?.current.condition.icon}
-        city={`${data?.location.name} ${data?.location.region} ${data?.location.country}`}
-        text={data?.current.condition.text}
-        temp={data?.current.feelslike_c}
-      />
+      <Loading value={isFetching}/>
+      <Main Day={data?.current.is_day}>
+        <InputSearch setSearch={setSearch}/>
+        <Weather 
+          urlIcon={data?.current.condition.icon}
+          city={`${data?.location.name} ${data?.location.region} ${data?.location.country}`}
+          text={data?.current.condition.text}
+          temp={data?.current.feelslike_c}
+        />
+      </Main>
+      
     </>
   )
 }
